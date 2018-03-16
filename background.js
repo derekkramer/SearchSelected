6f730153aaa3c483a70bf240e32c761ae02f6c4f
Derek Kramer <derekdevelops@gmail.com>
2018-03-15 19:59:14 -0600

    :tada: Search function working correctly

---
+++ background.js
@@ -0,0 +1,26 @@
+chrome.commands.onCommand.addListener(function(command) {
+    if(command) {
+        chrome.tabs.getSelected(null, function(tab) {
+            chrome.tabs.query({
+                active: true,
+                currentWindow: true
+            }, function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {
+                    action: "getSelection"
+                }, function(response) {
+                    var url = encodeGoogle(response.data.trim());
+
+                    chrome.tabs.create({ url: url });
+                });
+            });
+        });
+    }
+});
+
+function encodeGoogle(str) {
+    var ENGINE = 'https://www.google.com/search?q=';
+
+    var url = ENGINE + encodeURI(str.replace(/ /g, '+'));
+
+    return url;
+}
